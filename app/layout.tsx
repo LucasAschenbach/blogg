f8884f64965a886da285427ff0b733b1088a79d7
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-11-19 01:00:59 +0100

    Remove unused variable cmuserif

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Personal Blogg",
   description: "A personal blogg",
 };
-
-const cmuserif = localFont({
-  src: [
-    {
-      path: "../public/fonts/cmunrm.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/cmunti.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/cmunbx.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/cmunbi.ttf",
-      weight: "700",
-      style: "italic",
-    },
-  ],
-});
 
 export default function RootLayout({
   children,
